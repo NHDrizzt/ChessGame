3d75da17675ad424bfa9c707453a435372936196
Rokol <biel_s.carvalho2@hotmail.com>
2021-04-22 02:39:54 -0300

    Position

---
+++ src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+import board.Position;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Position p = new Position(3,5);
+		System.out.println(p);
+	}
+}
